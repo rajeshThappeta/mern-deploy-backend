aa543f3d00dbb3842d35697ba3b24ada7ea34926
rajesh <rajesh.t7982@gmail.com>
2024-07-11 18:35:22 +0530

    serverv10

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 const express = require('express');
 
 const app = express()
+const cors=require('cors');
 const PORT = 8000
+
+app.use(cors({
+  origin:'',
+  methods:['GET','POST','PUT','DELETE'],
+  credentials:true
+}))
 
 app.get('/', (req, res) => {
   res.send('Hello World')
 })
 
-app.get('/about', (req, res) => {
-  res.send('About route 🎉 ')
+app.get('/users', (req, res) => {
+  res.send([{id:1,name:"rvi"},{id:2,name:"bhanu"}])
 })
 
 app.listen(PORT, () => {
